3aa43ed62e7f17ebe197f9ec4f0708ad06cc5a23
wxilsq <qwxis181@gmail.com>
2026-04-22 22:54:25 +0300

    Refactor asyncFilterCallback for improved readability and implement asyncFilterPromise with abort signal handling

--- lab5/lab5.js
+++ lab5/lab5.js
@@ -6,9 +6,7 @@ function asyncFilterCallback(items, handler, done) {
         setTimeout(() => {
             handler(items[i], result => {
 
-                if (result) {
-                    passed.push(items[i]);
-                }
+                if (result) passed.push(items[i]);
 
                 processed++;
 
@@ -22,8 +20,55 @@ function asyncFilterCallback(items, handler, done) {
 }
 
 
-asyncFilterCallback(
-    [1, 2, 3, 4, 5],
-    (value, cb) => cb(value % 2 === 0),
-    result => console.log(result)
-);
+function asyncFilterPromise(items, handler, signal) {
+    return new Promise(async (resolve, reject) => {
+
+        let output = [];
+
+        for (const item of items) {
+
+            if (signal?.aborted) {
+                return reject("Stopped");
+            }
+
+            await new Promise(r => setTimeout(r, 200));
+
+            if (await handler(item)) {
+                output.push(item);
+            }
+        }
+
+        resolve(output);
+    });
+}
+
+
+asyncFilterPromise(
+    [10, 15, 20, 25],
+    value => value > 18
+).then(console.log);
+
+
+async function start() {
+    const data = await asyncFilterPromise(
+        [3, 6, 9, 12],
+        value => value % 3 === 0
+    );
+
+    console.log(data);
+}
+
+start();
+
+
+const controller = new AbortController();
+
+asyncFilterPromise(
+    [1,2,3,4,5,6],
+    value => value > 2,
+    controller.signal
+)
+.then(console.log)
+.catch(console.log);
+
+setTimeout(() => controller.abort(), 300);
