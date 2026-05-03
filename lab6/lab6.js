97e3ba61adf016634f2beaca2ba9369f547f5c36
wxilsq <qwxis181@gmail.com>
2026-05-03 22:40:37 +0300

    Implement line splitting and analysis functions for text file processing

--- lab6/lab6.js
+++ lab6/lab6.js
@@ -1,11 +1,45 @@
 const fs = require('fs');
 
-async function readChunks(filePath) {
-  const stream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+async function* splitToLines(stream) {
+  let buffer = '';
 
   for await (const chunk of stream) {
-    console.log(chunk);
+    buffer += chunk;
+    let parts = buffer.split('\n');
+
+    buffer = parts.pop();
+
+    for (let line of parts) {
+      yield line;
+    }
+  }
+
+  if (buffer) {
+    yield buffer;
   }
 }
 
-readChunks(__dirname + '/data.txt');
+async function analyze(filePath) {
+  const stream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+  let lines = 0;
+  let words = 0;
+  let longLines = 0;
+
+  for await (let line of splitToLines(stream)) {
+    lines++;
+
+    if (line.length > 25) {
+      longLines++;
+    }
+
+    const arr = line.trim().split(/\s+/);
+    words += arr.filter(Boolean).length;
+  }
+
+  console.log('Lines:', lines);
+  console.log('Words:', words);
+  console.log('Long lines:', longLines);
+}
+
+analyze(__dirname + '/data.txt').catch(console.error);
