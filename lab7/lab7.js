19381e4bde8694c7e166871c5af550822f02ff00
wxilsq <qwxis181@gmail.com>
2026-05-14 12:49:09 +0300

    Refactor LightBulb class to improve event handling and listener management

--- lab7/lab7.js
+++ lab7/lab7.js
@@ -9,12 +9,21 @@ class SmartHome extends EventEmitter {
 class LightBulb {
     constructor(room, home) {
         this.room = room;
+        this.home = home;
 
-        home.on("deviceOn", (device) => {
+        this.listener = (device) => {
             if (device === this.room) {
                 console.log("Light in " + this.room + " is now ON");
             }
-        });
+        };
+
+        this.home.on("deviceOn", this.listener);
+    }
+
+    disconnect() {
+        this.home.off("deviceOn", this.listener);
+
+        console.log("Light in " + this.room + " disconnected");
     }
 }
 
@@ -34,5 +43,13 @@ const home = new SmartHome();
 
 const kitchenLight = new LightBulb("kitchen", home);
 const kitchenAC = new AirConditioner("kitchen", home);
+
+home.on("deviceOn", (device) => {
+    console.log("[security] activity detected in " + device);
+});
+
+home.turnOn("kitchen");
+
+kitchenLight.disconnect();
 
 home.turnOn("kitchen");
