a0a9e06994410ff7c1219824310de37226293bc3
wxilsq <qwxis181@gmail.com>
2026-05-16 20:43:12 +0300

    Enhance AuthProxy class with request limit and JWT token renewal logic

--- lab8/main.js
+++ lab8/main.js
@@ -2,8 +2,16 @@ const AuthProxy = require('./proxy.js');
 
 async function run() {
     let myProxy = new AuthProxy("api_key", "secret_key_123");
-    let data = await myProxy.sendRequest("https://jsonplaceholder.typicode.com/todos/1");
-    console.log(data);
+    let data1 = await myProxy.sendRequest("https://jsonplaceholder.typicode.com/todos/1");
+    console.log("Result 1:", data1.id);
+
+    myProxy.changeStrategy("jwt", "expired_token_test");
+    
+    let data2 = await myProxy.sendRequest("https://jsonplaceholder.typicode.com/todos/2");
+    console.log("Result 2:", data2.id);
+    
+    let data3 = await myProxy.sendRequest("https://jsonplaceholder.typicode.com/todos/3");
+    console.log("Result 3:", data3.id);
 }
 
 run();
--- lab8/proxy.js
+++ lab8/proxy.js
@@ -2,9 +2,28 @@ class AuthProxy {
     constructor(authType, authData) {
         this.authType = authType;
         this.authData = authData;
+        this.requestsMade = 0;
+        this.limit = 5;
+    }
+
+    changeStrategy(newType, newData) {
+        this.authType = newType;
+        this.authData = newData;
+    }
+
+    async renewJwtToken() {
+        console.log("! Token expired, getting new one");
+        this.authData = "new_token_" + Math.floor(Math.random() * 1000);
     }
 
     async sendRequest(targetUrl, method = "GET", bodyData = null) {
+        this.requestsMade += 1;
+        
+        if (this.requestsMade > this.limit) {
+            console.log("Error: Too many requests");
+            return null;
+        }
+
         console.log("-> Request to", targetUrl);
 
         let reqHeaders = {
@@ -27,8 +46,15 @@ class AuthProxy {
         }
 
         let response = await fetch(targetUrl, options);
-        let result = await response.json();
 
+        if (response.status === 401 && this.authType === "jwt") {
+            await this.renewJwtToken();
+            reqHeaders["Authorization"] = "Bearer " + this.authData;
+            options.headers = reqHeaders;
+            response = await fetch(targetUrl, options);
+        }
+
+        let result = await response.json();
         return result;
     }
 }
